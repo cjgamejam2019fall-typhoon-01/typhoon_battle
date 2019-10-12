36e83fb1417acbd56a6298819039d87cb580a7a2
cjgamejam2019fall-typhoon-01 <cjgamejam2019fall.typhoon@gmail.com>
2019-10-13 05:30:41 +0900

    台風が弱くなるようになった

--- Assets/Apps/Scripts/Actor/Typhoon/Typhoon.cs
+++ Assets/Apps/Scripts/Actor/Typhoon/Typhoon.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Apps.Actor.Typhoon
 {
     public class Typhoon : MonoBehaviour
     {
+        public Vector2 PowerRange = Vector2.one * 900f;
+        private Vector3 InitialLocalScale = Vector3.one;
+        [System.NonSerialized]
         public float Power = 900f;
+        private float InitialPower = 900f;
         public float PowerRatio = 1f;
         public float EffectiveRange = 50f;
         public float MoveSpeedRatio = 1f;
         public float TargetCheckRadius = 10000f;
+        public Vector2 ScaleShrinkRatioRange = Vector2.one;
+        private float ScaleShrinkRatio = 1.0f;
         private List<VectorField.VectorField.VectorInfo> _NearVecorInfoList = new List<VectorField.VectorField.VectorInfo>();
 
         private void Awake()
         {
             GatherNearVectorInfo();
+            InitialPower = Power = Random.Range(PowerRange.x, PowerRange.y);
+            ScaleShrinkRatio = Random.Range(ScaleShrinkRatioRange.x, ScaleShrinkRatioRange.y);
+        }
+
+        private void Start()
+        {
+            InitialLocalScale = transform.localScale;
         }
 
         private void Update()
@@ -35,7 +47,22 @@ namespace Apps.Actor.Typhoon
                 transform.rotation = rotation;
             }
 
+            // 縮小
+            Power = Mathf.Max(Power - Time.deltaTime * ScaleShrinkRatio, 0);
+            var scaleRate = Power / InitialPower;
+            transform.localScale = InitialLocalScale * scaleRate;
+            if (Power <= 0)
+            {
+                Destroy(gameObject);
+            }
+
             // ターゲット判定
+#if UNITY_EDITOR
+            if (GameSystem.GameManager.Level == null)
+            {
+                return;
+            }
+#endif
             var targetCities = GameSystem.GameManager.Level.TargetCities;
             foreach (var city in targetCities)
             {
@@ -44,6 +71,10 @@ namespace Apps.Actor.Typhoon
                     city.EatDamage(Time.deltaTime * Power * PowerRatio);
                 }
             }
+        }
+
+        private void LateUpdate()
+        {
         }
 
         private void GatherNearVectorInfo()
